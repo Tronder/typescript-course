323747d7f97b188d726521c5f1f20db43b2f9d0d
Tronder <trond.erland@gmail.com>
2023-01-24 14:49:12 +0100

    continue

--- app.ts
+++ app.ts
@@ -1,4 +1,17 @@
-function combine(n1: number, n2: number){
-    const result = n1 +n2;
-    return result;
+function add(n1: number, n2: number){
+    return n1 + n2;
 }
+
+function printResult(num:number): void {
+    console.log('result: ' + num);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b:number) => number;
+
+combineValues = add;
+combineValues = printResult;
+
+
+console.log(combineValues(8, 8));
